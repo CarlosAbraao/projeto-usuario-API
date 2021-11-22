b1ae454d7094ecf56f3d7b37790f39bc53de51cf
Carlos Abraão <carloschavesnd@hotmail.com>
2021-11-22 11:23:26 -0300

    Inicio do projeto

---
+++ src/main/java/com/devcarlos/usuarioAPI/ProjetoUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlos.usuarioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUsuarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcarlos/usuarioAPI/ProjetoUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlos.usuarioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUsuarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
