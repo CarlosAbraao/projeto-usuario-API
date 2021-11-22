429dce5af048452ba1f4e1910bddb1fed9c6eb28
Carlos Abraão <carloschavesnd@hotmail.com>
2021-11-22 16:11:01 -0300

    Inserindo um usuario no banco através de um get
    INSERE UM USUARIO NO BANCO SEMPRE QUE O USUARIO CARREGAR A PÁGINA (OU SEJA FAZER UM GET DE ALGO)
    		// SE FOR CRIAR VARIOS USUARIOS É MELHOR CRIAR UMA LISTA DE USUARIOS
    		// TODA VEZ QUE O USUARIO FAZ O GET DA PÁGINA A INFORMAÇÃO É REPETIDA NO BANCO

--- src/main/java/com/devcarlos/usuarioAPI/controllers/HomePageController.java
+++ src/main/java/com/devcarlos/usuarioAPI/controllers/HomePageController.java
@@ -19,7 +19,10 @@ public class HomePageController {
 
 	@GetMapping("/")
 	public String HomePage() {
-		
+		// INSERE UM USUARIO NO BANCO SEMPRE QUE O USUARIO CARREGAR A PÁGINA (OU SEJA FAZER UM GET DE ALGO)
+		// SE FOR CRIAR VARIOS USUARIOS É MELHOR CRIAR UMA LISTA DE USUARIOS
+		// TODA VEZ QUE O USUARIO FAZ O GET DA PÁGINA A INFORMAÇÃO É REPETIDA NO BANCO
+
 		UsuarioEntity usuarioEntity = new UsuarioEntity();
 		usuarioEntity.setNome("carlos");
 		usuarioEntity.setIdade(10);
