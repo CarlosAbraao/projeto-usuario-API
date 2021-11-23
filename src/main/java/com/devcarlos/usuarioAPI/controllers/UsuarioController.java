0514355efaaa53ac4220cfd719bcb8b5c103ea5f
Carlos Abraão <carloschavesnd@hotmail.com>
2021-11-23 10:33:37 -0300

    Criando pagina lista_usuarios com ajuda do bootstrap

--- src/main/java/com/devcarlos/usuarioAPI/controllers/UsuarioController.java
+++ src/main/java/com/devcarlos/usuarioAPI/controllers/UsuarioController.java
@@ -1,14 +1,21 @@
 package com.devcarlos.usuarioAPI.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-
+   @GetMapping("/")
     public String listarUsuarios(){
         return "usuarios_listar";
+    }
+
+    @PutMapping("/add")
+    public String adicionarUsuarios(){
+        return "usuarios_add";
     }
 }
